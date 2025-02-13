c8e36e23df3ccbd2ca5d887edfe28ff512ddb5cc
Rafael Castro <rafaelpereiradecastro06@gmail.com>
2025-02-13 19:55:06 -0300

    Projeto cadastro Usuario

---
+++ Projeto Cadastro Usuario/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css"
+import React from "react"
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Rafael<span className="text-danger">CTV</span></strong>
+        </span>
+    </footer>
